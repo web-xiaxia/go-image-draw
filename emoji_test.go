a9116433c644bf5a6e345c905c378e06ec44adca
熊闯 <xiongchuang@vip.qq.com>
2023-08-19 23:19:59 +0800

    init

--- emoji_test.go
+++ emoji_test.go
@@ -1,14 +1,6 @@
 package go_image_draw
 
 import (
-	"bytes"
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
-	"github.com/davidbyttow/govips/v2/vips"
-	"io/ioutil"
-	"os"
-	"strings"
 	"testing"
 )
 
@@ -17,52 +9,4 @@ func TestInitEmojiImageMap(t *testing.T) {
 	if err != nil {
 		t.Error(err)
 	}
-}
-
-func TestEmojiJson(t *testing.T) {
-	dirName := "/Users/xiaxia/web/dev/w/xemoji/image"
-	dirEntries, err := os.ReadDir(dirName)
-	if err != nil {
-		return
-	}
-	ret := make(map[string]string, len(dirEntries))
-	for _, dirEntry := range dirEntries {
-		nameArr := strings.Split(dirEntry.Name(), ".")
-		if len(nameArr) != 2 || nameArr[1] != "png" {
-			continue
-		}
-
-		file, err := os.Open(fmt.Sprintf("%s/%s", dirName, dirEntry.Name()))
-		if err != nil {
-			return
-		}
-		defer func(f *os.File) {
-			_ = f.Close()
-		}(file)
-		all, err := ioutil.ReadAll(file)
-		if err != nil {
-			return
-		}
-		reader, err := vips.NewImageFromReader(bytes.NewBuffer(all))
-		if err != nil {
-			panic(fmt.Sprintf("%s:%+v", nameArr[0], err))
-		}
-		pngImage, _, err := reader.ExportPng(&vips.PngExportParams{
-			StripMetadata: false,
-			Quality:       100,
-		})
-		if err != nil {
-			panic(fmt.Sprintf("%s:%+v", nameArr[0], err))
-		}
-		ret[nameArr[0]] = base64.StdEncoding.EncodeToString(pngImage)
-	}
-	marshal, err := json.Marshal(ret)
-	if err != nil {
-		panic(err)
-	}
-	err = os.WriteFile("emoji.json", marshal, 666)
-	if err != nil {
-		panic(err)
-	}
-
 }
--- go.mod
+++ go.mod
@@ -4,13 +4,7 @@ go 1.17
 
 require (
 	github.com/SubLuLu/grapheme-splitter v0.0.0-20190108090012-1c706d78ac26
-	github.com/davidbyttow/govips/v2 v2.13.0
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	golang.org/x/image v0.11.0
-)
-
-require (
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
 )
